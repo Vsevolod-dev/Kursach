ed100641f989c534a5e96cd78cacce410b0a5d64
mokonair <47756011+mokonair@users.noreply.github.com>
2020-06-01 04:21:00 +0300

    Создал нужные формы
    Дочерняя открывается, но данные не берёт ещё. Сделаю обработку данных завтра днём/ночью.

---
+++ kamparmok/MDIparent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kamparmok
+{
+    public partial class MDIparent : Form
+    {
+        public MDIparent()
+        {
+            InitializeComponent();
+        }
+        //Открыть в MenuStrip
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //
+            TableForm tableForm = new TableForm();
+            tableForm.MdiParent = this;
+            tableForm.Show();
+            tableForm.Open_Click(this, null); //метод на TableForm
+        }
+        //Создать в MenuStrip
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Сохранить в MenuStrip
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Сохранить как.. в MenuStrip
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Выход в MenuStrip
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
