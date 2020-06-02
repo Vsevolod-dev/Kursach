5d7a6004c4a671f287c15a42414fd080fa38f546
Vsevolod-dev <60555096+Vsevolod-dev@users.noreply.github.com>
2020-06-03 00:19:34 +0300

    Почти сделали нормальное распределение
    Посчитали интервалы и частоты, нашли среднее значение и стандартное отклонение
    Осталось найте теоритические частоты и что то дальше после них

--- kamparmok/TableForm.Designer.cs
+++ kamparmok/TableForm.Designer.cs
@@ -36,8 +36,15 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridViewTable = new System.Windows.Forms.DataGridView();
+            this.calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewIntervals = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIntervals)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -46,7 +53,7 @@
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1056, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -65,35 +72,35 @@
             // createToolStripMenuItem
             // 
             this.createToolStripMenuItem.Name = "createToolStripMenuItem";
-            this.createToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.createToolStripMenuItem.Text = "Создать";
             this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.openToolStripMenuItem.Text = "Открыть";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.saveToolStripMenuItem.Text = "Сохранить";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.saveAsToolStripMenuItem.Text = "Сохранить как";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.exitToolStripMenuItem.Text = "Выход";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -103,14 +110,73 @@
             this.dataGridViewTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewTable.Location = new System.Drawing.Point(12, 27);
             this.dataGridViewTable.Name = "dataGridViewTable";
-            this.dataGridViewTable.Size = new System.Drawing.Size(413, 411);
+            this.dataGridViewTable.Size = new System.Drawing.Size(763, 411);
             this.dataGridViewTable.TabIndex = 1;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(781, 27);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 2;
+            this.calculate.Text = "Рассчитать";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.Calculate_Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(821, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(881, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(878, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите количество интервалов";
+            // 
+            // dataGridViewIntervals
+            // 
+            this.dataGridViewIntervals.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewIntervals.Location = new System.Drawing.Point(12, 467);
+            this.dataGridViewIntervals.Name = "dataGridViewIntervals";
+            this.dataGridViewIntervals.Size = new System.Drawing.Size(763, 241);
+            this.dataGridViewIntervals.TabIndex = 7;
+            this.dataGridViewIntervals.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 451);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Интервалы и частоты";
             // 
             // TableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1056, 782);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewIntervals);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculate);
             this.Controls.Add(this.dataGridViewTable);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -119,6 +185,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIntervals)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,5 +201,11 @@
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.DataGridView dataGridViewTable;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewIntervals;
+        private System.Windows.Forms.Label label3;
     }
 }
--- kamparmok/TableForm.cs
+++ kamparmok/TableForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,12 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace kamparmok
 {
     public partial class TableForm : Form
     {
         string file_name;
+        string[] rows;
+        int rowsCount;
+        int columnReg;
+        int colCount;
+
         public TableForm()
         {
             InitializeComponent();
@@ -48,24 +56,139 @@ namespace kamparmok
             //Чистим таблицу
             dataGridViewTable.Rows.Clear();
             dataGridViewTable.Columns.Clear();
-            for (int i = 0; i < 100; i++)
-            {
-                dataGridViewTable.Columns.Add("", "");
-                dataGridViewTable.Rows.Add();
-            }
-            //Открываем OpenFileDialog
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = ".CSV файлы (*.csv)|*.csv|All files(*.*)|*.*";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 file_name = openFileDialog.FileName;
                 this.Text = "Файл: " + file_name;
+                FileStream file = new FileStream(file_name, FileMode.OpenOrCreate, FileAccess.Read); //переменная для чтения и записи
 
+                StreamReader StreamReader = new StreamReader(file); //переменная для посимвольного считывания
+
+                Regex reg = new Regex(";"); //разделитель между ячейками (регулярные выражения)
+
+                string full_text = StreamReader.ReadToEnd(); //считывание всех символов в файле и записаль в в переменную
+
+                MatchCollection count = reg.Matches(full_text); //поиск reg(;) в full_text и записывает их кол-во в переменную
+
+                columnReg = Convert.ToInt32(count.Count.ToString()); //перевод count в int'овый вариант
+
+                rows = File.ReadAllLines(file_name); //запись строк
+
+                rowsCount = rows.Length; // кол-во строк
+
+                colCount = columnReg / rowsCount + 1; // кол-во столбцов
+                for (int i = 0; i < colCount; ++i) //создание столбцов
+                {
+                    dataGridViewTable.Columns.Add("", "");
+                }
+
+                //когда мы начинаем заполнять файл, это происходит построчно
+
+                for (int i = 0; i < rowsCount; ++i) //создание строк ( с разделителем???? ) цикл работает 5 раз?
+                {
+                    dataGridViewTable.Rows.Add(rows[i].Split(';'));
+                }
+                StreamReader.Close();
             }
             else
             {
 
             }
+        }
+
+        private void Calculate_Button_Click(object sender, EventArgs e)
+        {
+            double max = Convert.ToDouble(dataGridViewTable[0, 0].Value);
+            double min = Convert.ToDouble(dataGridViewTable[0, 0].Value);
+
+            for (int i = 0; i < rowsCount; ++i) //поиск мниимального и максимального значения
+            {
+                for (int j = 0; j <colCount; ++j)
+                {
+                    if (Convert.ToDouble(dataGridViewTable[i, j].Value) >= max) //максимум
+                    {
+                        max = Convert.ToDouble(dataGridViewTable[i, j].Value);
+                    }
+                    if (Convert.ToDouble(dataGridViewTable[i, j].Value) <= min) //минимум
+                    {
+                        min = Convert.ToDouble(dataGridViewTable[i, j].Value);
+                    }
+                }
+            }
+
+            label1.Text = dataGridViewTable[2, 2].Value.ToString() + min + "\n" + max;
+
+            double rangeOfVariation = max - min; //размах вариации
+
+            int numberOfIntervals = Convert.ToInt32(textBox1.Text); //количество интервалов
+
+            double step = rangeOfVariation / numberOfIntervals; //длина интервала
+
+            dataGridViewIntervals.Columns.Add("", "");
+            dataGridViewIntervals.Columns.Add("", "");
+
+            for (int i = 0; i < numberOfIntervals; ++i) //рассчёт интервалов
+            {
+                dataGridViewIntervals.Rows.Add(min, Math.Round(min + step, 2));
+                min = Convert.ToDouble(dataGridViewIntervals[1, i].Value);
+            }
+
+            dataGridViewIntervals.Columns.Add("", "Частоты"); //третий столбец для частот [2, i]
+            dataGridViewIntervals.Columns.Add("", "Середины интервалов"); //четвертый столбец для середин интервалов [3, i]
+
+            double leftBorder, rightBorder; //границы интервалов
+            double middleValue = 0; //среднее значение
+            int frequencyCount;
+
+            for (int i = 0; i < numberOfIntervals; ++i) //рассчёт частот
+            {
+                frequencyCount = 0; //частота
+                leftBorder = Convert.ToDouble(dataGridViewIntervals[0, i].Value); 
+                rightBorder = Convert.ToDouble(dataGridViewIntervals[1, i].Value);
+                for (int k = 0; k < rowsCount; ++k)
+                {
+                    for (int j = 0; j < colCount; ++j)
+                    {
+                        if (Convert.ToDouble(dataGridViewTable[k, j].Value) >= leftBorder && Convert.ToDouble(dataGridViewTable[k, j].Value) <= rightBorder)
+                        {
+                            frequencyCount++;
+                        }
+                    }
+                }
+                dataGridViewIntervals[3, i].Value = (rightBorder + leftBorder) / 2;
+                dataGridViewIntervals[2, i].Value = frequencyCount;
+                middleValue += Convert.ToDouble(dataGridViewIntervals[3, i].Value) * Convert.ToDouble(dataGridViewIntervals[2, i].Value);
+            }
+
+            middleValue /= 100;
+            label1.Text += " " + middleValue;
+
+            double sumHelp = 0;
+
+            dataGridViewIntervals.Columns.Add("", "Вспомогательный столбец"); //четвертый столбец для середин интервалов [4, i]
+
+            for (int i = 0; i < numberOfIntervals; ++i) //рассчёт частот
+            {
+                for (int k = 0; k < rowsCount; ++k)
+                {
+                    for (int j = 0; j < colCount; ++j)
+                    {
+                        dataGridViewIntervals[4, i].Value = Math.Pow(Convert.ToDouble(dataGridViewIntervals[3, i].Value) - middleValue, 2) * Convert.ToDouble(dataGridViewIntervals[2, i].Value);
+                        
+                    }
+                }
+                sumHelp += Convert.ToDouble(dataGridViewIntervals[4, i].Value);
+            }
+
+            double stdDev = Math.Sqrt(sumHelp / (rowsCount * colCount));
+            }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
