63510a1a704b28c5a7e44e2b69884c7d895b25c2
mokonair <47756011+mokonair@users.noreply.github.com>
2020-06-02 03:48:03 +0300

    Ничо не работает, надо менять

--- kamparmok/TableForm.cs
+++ kamparmok/TableForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace kamparmok
 {
@@ -58,9 +60,45 @@ namespace kamparmok
             openFileDialog.Filter = ".CSV файлы (*.csv)|*.csv|All files(*.*)|*.*";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                file_name = openFileDialog.FileName;
-                this.Text = "Файл: " + file_name;
+                try
+                {
+                    file_name = openFileDialog.FileName;
+                    this.Text = "Файл: " + file_name;
+                    FileStream fileStream = new FileStream(file_name, FileMode.OpenOrCreate, FileAccess.Read);
+                    StreamReader streamReader = new StreamReader(fileStream);
+                    Regex regex = new Regex(";");
+                    //string s = streamReader.ReadToEnd();
+                    //MatchCollection aa = regex.Matches(s);
+                    //string a = aa.Count.ToString();
+                    //int aint = Convert.ToInt32(a);
+                    string[] rows = File.ReadAllLines(openFileDialog.FileName);
+                    //int art = rows.Length, art1 = rows.Length - Convert.ToInt32(rows[art - 1]) - Convert.ToInt32(rows[art - 2]) - 2;
+                    //int x = aint / art1;
+                    //for (int i = 0; i < x; i++)
+                    //{
+                    //    dataGridViewTable.Columns.Add("", "");
+                    //}
+                    //for (int i = 0; i < art1 - 1; i++)
+                    //{
+                    //    dataGridViewTable.Rows.Add(rows[i].Split(';'));
+                    //}
 
+                    //for (int i = 0; i < Convert.ToInt32(rows[art - 1]); i++)
+                    //{
+                    //    dataGridViewTable.Columns[i].HeaderText = rows[i + Convert.ToInt32(rows[art - 2])];
+
+                    //}
+                    //for (int i = 0; i < Convert.ToInt32(rows[art - 2]); i++)
+                    //{
+                    //    dataGridViewTable.Rows[i].HeaderCell.Value = rows[i + Convert.ToInt32(rows[art - 1]) + Convert.ToInt32(rows[art - 2])];
+
+                    //}
+                    streamReader.Close();
+                }
+                catch (Exception Situation) 
+                { 
+                    MessageBox.Show(Situation.Message, "Error", MessageBoxButtons.OK); 
+                }
             }
             else
             {
